f31bde5aef2f51d0ef7cfda9fe5e820e62bef408
samurai-gonshim <gonshim@gmail.com>
2014-06-15 22:19:44 +0900

    add tag polymorphic

--- app/views/blogs/index.blade.php
+++ app/views/blogs/index.blade.php
@@ -8,9 +8,15 @@
 
     <dl>
     @foreach($blogs as $blog) 
-	    <dt> <h3>{{ $blog->title }}<h3> </dt>
+	    <dt> <h3>{{ $blog->title }} <h3> </dt>
+	    <p> posted by {{ $blog->user->username }} </p>
+	    <p> created at {{ $blog->created_at }} </p>
 	    <dd>
 	   	<p class="bg-info"> {{ $blog->body }} </p>
+	   	<h4>Tags : </h4>
+	   	@foreach ($blog->tags as $tag) 
+	   		<p> {{ $tag->tag }} </p>
+	   	@endforeach
 	   	@foreach($blog->comments as $comment)
 	   		<blockquote>
 	   		<p>{{ $comment->comment }}</p>
